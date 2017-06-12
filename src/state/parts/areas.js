773929a640668c3f16479b18eaf100a6e4454c73
Artur Falborski <falborski.artur@gmail.com>
2017-06-12 20:08:44 +0200

    reducer for areas.json file

---
+++ src/state/parts/areas.js
@@ -0,0 +1,27 @@
+const initialState = {
+  data: null,
+  fetching: false,
+  error: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'areas/FETCH__BEGIN':
+      return {
+          ...state,
+        fetching: true
+      }
+    case 'areas/FETCH__SUCCESS':
+      return {
+          ...state,
+        fetching: false,
+        data: action.data
+      }
+    case 'areas/FETCH__FAILURE':
+      return {
+          ...state,
+        fetching: false,
+        error: action.error
+      }
+  }
+}
--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,9 +1,11 @@
 import playerStats from './parts/playerStats'
 import playView from './parts/playView'
+import areas from './parts/areas'
 
 export default (state = {}, action) => {
   return {
     playerStats: playerStats(state.playerStats, action),
-    playView: playView(state.playView, action)
+    playView: playView(state.playView, action),
+    areas: areas(state.areas, action)
   }
 }
