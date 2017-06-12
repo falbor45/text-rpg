c04bee179c971ec54ce1e4a3716ec84d2ab6d93f
Artur Falborski <falborski.artur@gmail.com>
2017-06-12 15:48:50 +0200

    player view with 3 column width

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {Grid} from 'react-bootstrap'
 import PlayerView from './PlayerView'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Grid>
         <PlayerView/>
+        </Grid>
       </div>
     );
   }
--- src/PlayerView.js
+++ src/PlayerView.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import { Col } from 'react-bootstrap'
 
 class PlayerView extends Component {
   render() {
     return (
-        <div>
+        <Col lg={3}>
           <h1>Player</h1>
           <h2>Health</h2>
           <p>100/100</p>
           <h2>Energy</h2>
           <p>20/20</p>
-        </div>
+        </Col>
     )
   }
 }
