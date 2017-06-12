5ca5d283f2b2e5bd00bf124590968ca00d7e57eb
Artur Falborski <falborski.artur@gmail.com>
2017-06-12 19:26:35 +0200

    PlayView component connected to store + style tweaks

--- src/PlayView.js
+++ src/PlayView.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import { Col } from 'react-bootstrap'
+import { connect } from 'react-redux'
 
+export default connect(
+    state => ({
+      playerStats: state.playerStats
+    }),
+    dispatch => ({
+      hitPlayer: value => dispatch({ type: 'playerStats/HIT_PLAYER', value}),
+      inputChange: value => dispatch({ type: 'playView/INPUT_CHANGE', value})
+    })
+)(
 class PlayView extends Component {
   storyOutputStyle = {
     width: "100%",
@@ -10,17 +20,23 @@ class PlayView extends Component {
   }
 
   playerInputStyle = {
-    width: "100%",
+    width: "75%",
   }
+
+  playerButtonStyle = {
+    width: '24%',
+    float: 'right'
+  }
+
   render() {
     return (
         <Col lg={6}>
           <hr/>
           <textarea style={this.storyOutputStyle} readOnly value=""/>
           <input style={this.playerInputStyle}/>
+          <button style={this.playerButtonStyle}>Perform action</button>
         </Col>
     )
   }
 }
-
-export default PlayView
+)
