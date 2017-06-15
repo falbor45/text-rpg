7e6112dbd9f5f98de744d0419308cddcf6e80bd3
Artur Falborski <falborski.artur@gmail.com>
2017-06-15 11:27:13 +0200

    display either enemy encounter or choice events based on RNG (WIP)

--- src/PlayView.js
+++ src/PlayView.js
@@ -7,7 +7,8 @@ export default connect(
       playerStats: state.playerStats,
       playView: state.playView,
       areas: state.areas,
-      enemies: state.enemies
+      enemies: state.enemies,
+      choiceEvents: state.choiceEvents
     }),
     dispatch => ({
       hitPlayer: value => dispatch({ type: 'playerStats/HIT_PLAYER', value}),
@@ -18,6 +19,9 @@ export default connect(
       enemiesFetchBegin: () => dispatch({ type: 'enemies/FETCH__BEGIN'}),
       enemiesFetchSuccess: data => dispatch({ type: 'enemies/FETCH__SUCCESS', data}),
       enemiesFetchFailure: error => dispatch({ type: 'enemies/FETCH__FAILURE', error}),
+      choiceEventsFetchBegin: () => dispatch({ type: 'choiceEvents/FETCH__BEGIN'}),
+      choiceEventsFetchSuccess: data => dispatch({ type: 'choiceEvents/FETCH__SUCCESS', data}),
+      choiceEventsFetchFailure: error => dispatch({ type: 'choiceEvents/FETCH__FAILURE', error})
     })
 )(
 class PlayView extends Component {
@@ -48,6 +52,19 @@ class PlayView extends Component {
     ).catch(
         error => this.props.enemiesFetchFailure('Connection error.')
     )
+
+    this.props.choiceEventsFetchBegin()
+    fetch(
+        `${process.env.PUBLIC_URL}/data/choiceEvents.json`
+    ).then(
+        response => response.json().then(
+            data => this.props.choiceEventsFetchSuccess(data)
+        ).catch(
+            error => this.props.choiceEventsFetchFailure('Malformed JSON.')
+        )
+    ).catch(
+        error => this.props.choiceEventsFetchFailure('Connection error.')
+    )
   }
 
   storyOutputStyle = {
@@ -68,15 +85,18 @@ class PlayView extends Component {
 
   state = {
     storyOutput: [],
+    events: ['fight', 'choiceEvent'],
     areaRNG: 0,
-    enemyRNG: 0
+    enemyRNG: 0,
+    eventRNG: 0
   }
 
   handleStoryUpdate = () => this.setState ({
     areaRNG: Math.floor(Math.random() * this.props.areas.data.length),
-    enemyRNG: Math.floor(Math.random() * this.props.enemies.data.length),
-    storyOutput: this.state.storyOutput.concat(this.props.areas.data[this.state.areaRNG].description,
-        'You encounter ' + this.props.enemies.data[this.state.enemyRNG].name + '!'),
+    eventRNG: Math.floor(Math.random() * this.state.events.length),
+    enemyRNG: this.state.events[this.state.eventRNG] === 'fight' ? Math.floor(Math.random() * this.props.enemies.data.length) : 0,
+    storyOutput: this.state.events[this.state.eventRNG] === 'fight' ? this.state.storyOutput.concat(this.props.areas.data[this.state.areaRNG].description,
+        'You encounter ' + this.props.enemies.data[this.state.enemyRNG].name + '!') : this.state.events[this.state.eventRNG] === 'choiceEvent' ? this.state.storyOutput.concat(this.props.choiceEvents.data[this.state.choiceEventRNG].description) : null,
   })
 
   render() {
