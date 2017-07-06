c60f69babd14c9aedd086becfe198fcf73a2bf58
Artur Falborski <falborski.artur@gmail.com>
2017-07-06 16:43:43 +0200

    read enemy stats from store

--- src/EnemyView.js
+++ src/EnemyView.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import { Col } from 'react-bootstrap'
+import { connect } from 'react-redux'
 
-class EnemyView extends Component {
-  render() {
-    return (
-        <Col lg={3} lgPush={1}>
-          <h1>Monster</h1>
-          <h2>Health</h2>
-          <p>12/12</p>
-          <h2>Energy</h2>
-          <p>5/5</p>
-        </Col>
-    )
-  }
-}
-
-export default EnemyView
+export default connect(
+    state => ({
+      enemyStats: state.enemyStats
+    }),
+    dispatch => ({
+    })
+)(
+    class EnemyView extends Component {
+      render() {
+        return (
+            <Col lg={3} lgPush={1}>
+              <h1>Monster</h1>
+              <h2>Health</h2>
+              <p>{this.props.enemyStats.health}/{this.props.enemyStats.maxHealth}</p>
+              <h2>Energy</h2>
+              <p>{this.props.enemyStats.energy}/{this.props.enemyStats.maxEnergy}</p>
+            </Col>
+        )
+      }
+    }
+)
