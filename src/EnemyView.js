8e3bb9a493f94ca12b305f9ad695e8a71e8d1c94
Artur Falborski <falborski.artur@gmail.com>
2017-07-09 20:20:22 +0200

    show enemyView component depending whether enemy is alive or not

--- src/EnemyView.js
+++ src/EnemyView.js
@@ -13,11 +13,16 @@ export default connect(
       render() {
         return (
             <Col lg={3} lgPush={1}>
-              <h1>Monster</h1>
-              <h2>Health</h2>
-              <p>{this.props.enemyStats.health}/{this.props.enemyStats.maxHealth}</p>
-              <h2>Energy</h2>
-              <p>{this.props.enemyStats.energy}/{this.props.enemyStats.maxEnergy}</p>
+                {
+                    this.props.enemyStats.isAlive ?
+                        <div>
+                            <h1>Monster</h1>
+                            <h2>Health</h2>
+                            <p>{this.props.enemyStats.health}/{this.props.enemyStats.maxHealth}</p>
+                            <h2>Energy</h2>
+                            <p>{this.props.enemyStats.energy}/{this.props.enemyStats.maxEnergy}</p>
+                        </div> : <div></div>
+                }
             </Col>
         )
       }
