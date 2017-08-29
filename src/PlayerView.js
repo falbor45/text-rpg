028504f15e71447d7b3d2a41fbe1cf5d7aa88b1d
Artur Falborski <falborski.artur@gmail.com>
2017-08-29 21:02:16 +0200

    dynamically display attributes and attack power in player HUD

--- src/PlayerView.js
+++ src/PlayerView.js
@@ -8,6 +8,13 @@ export default connect (
     }),
 )(
     class PlayerView extends Component {
+
+      attrStyleLeft = {
+        float: 'left'
+      }
+      attrStyleRight = {
+        float: 'right'
+      }
       render() {
         return (
             <Col lg={3}>
@@ -16,6 +23,21 @@ export default connect (
               <p>{`${this.props.playerStats.health}/${this.props.playerStats.maxHealth}`}</p>
               <h2>Energy</h2>
               <p>{`${this.props.playerStats.energy}/${this.props.playerStats.maxEnergy}`}</p>
+              <h2>Attack power: {this.props.playerStats.attackPower}</h2>
+              <hr/>
+              <h2>Attributes</h2>
+              <div style={this.attrStyleLeft}>
+              <h3>Strength:</h3>
+              <h3>Wisdom:</h3>
+              <h3>Agility:</h3>
+              <h3>Constitution:</h3>
+              </div>
+              <div style={this.attrStyleRight}>
+              <h3>{this.props.playerStats.strength}</h3>
+              <h3>{this.props.playerStats.wisdom}</h3>
+              <h3>{this.props.playerStats.agility}</h3>
+              <h3>{this.props.playerStats.constitution}</h3>
+              </div>
             </Col>
         )
       }
