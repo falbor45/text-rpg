d080cf7a581739e2f5858011a1673e270efef263
Falborski Artur <falborski.artur@gmail.com>
2017-09-15 15:26:53 +0200

    swap between attributes/details sections with buttons

--- src/PlayerView.js
+++ src/PlayerView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Col, ProgressBar } from 'react-bootstrap'
+import { Col, ProgressBar, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import blockFront1 from './assets/images/blockfront1.png'
 import blockFront2 from './assets/images/blockfront2.png'
@@ -73,6 +73,10 @@ export default connect (
         this.props.playerStatsGainStatPoint(-1)
       }
 
+      state = {
+        viewedTab: 'attributes'
+      }
+
       addPoint = {
         cursor: 'pointer',
         border: '1px solid black',
@@ -119,32 +123,42 @@ export default connect (
                 <p style={this.barOverlap}>{`${this.props.playerStats.experience}/${this.props.playerStats.maxExperience}`}</p>
               </div>
               <ProgressBar bsStyle="warning" now={Math.round((this.props.playerStats.experience / this.props.playerStats.maxExperience) * 100)}/>
-              <h2>Attack power</h2>
-              <h3>{this.props.playerStats.attackPowerMin} - {this.props.playerStats.attackPowerMax}</h3>
-              <h2>Dodge chance</h2>
-              <h3>{this.props.playerStats.baseDodgeChance}%</h3>
-              <h2>Armour</h2>
-              <h3>{this.props.playerStats.armour}</h3>
-              <p>Damage reduction: {this.props.playerStats.damageReduction}%</p>
               <h2>Block setup </h2>
               <img onMouseDown={(e) => this.handleLeftBlockClick(e)} src={this.props.blockMechanic.leftBlockPoints === 1 ? blockLeft1 : this.props.blockMechanic.leftBlockPoints === 2 ? blockLeft2 : this.props.blockMechanic.leftBlockPoints === 3 ? blockLeft3 : blockPlaceholder} style={this.imageBlockStyle}/>
               <img onMouseDown={(e) => this.handleFrontBlockClick(e)} src={this.props.blockMechanic.frontBlockPoints === 1 ? blockFront1 : this.props.blockMechanic.frontBlockPoints === 2 ? blockFront2 : this.props.blockMechanic.frontBlockPoints === 3 ? blockFront3 : blockPlaceholder} style={this.imageBlockStyle}/>
               <img onMouseDown={(e) => this.handleRightBlockClick(e)} src={this.props.blockMechanic.rightBlockPoints === 1 ? blockRight1 : this.props.blockMechanic.rightBlockPoints === 2 ? blockRight2 : this.props.blockMechanic.rightBlockPoints === 3 ? blockRight3 : blockPlaceholder} style={this.imageBlockStyle}/>
               <p>Points undistributed: {this.props.blockMechanic.unusedBlockPoints}</p>
               <hr/>
-              <h2>Attributes</h2>
-              <Col xs={6} sm={6} md={6} lg={7}>
-              <h3>Strength:</h3>
-              <h3>Wisdom:</h3>
-              <h3>Agility:</h3>
-              <h3>Constitution:</h3>
-              </Col>
-              <Col xs={6} sm={6} md={6} lgPush={1} lg={4}>
-                <h3>{this.props.playerStats.strength} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('str')} style={this.addPoint}>+</span> : null}</h3>
-              <h3>{this.props.playerStats.wisdom} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('wis')} style={this.addPoint}>+</span> : null}</h3>
-              <h3>{this.props.playerStats.agility} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('agi')} style={this.addPoint}>+</span> : null}</h3>
-              <h3>{this.props.playerStats.constitution} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('cons')} style={this.addPoint}>+</span> : null}</h3>
-              </Col>
+              <Button onClick={() => this.setState({viewedTab: 'attributes'})}>Attributes</Button>
+              <Button onClick={() => this.setState({viewedTab: 'details'})}>Details</Button>
+              {this.state.viewedTab === 'attributes' ? (
+                <div>
+                  <h2>Attributes</h2>
+                  <Col xs={6} sm={6} md={6} lg={7}>
+                    <h3>Strength:</h3>
+                    <h3>Wisdom:</h3>
+                    <h3>Agility:</h3>
+                    <h3>Constitution:</h3>
+                  </Col>
+                  <Col xs={6} sm={6} md={6} lgPush={1} lg={4}>
+                    <h3>{this.props.playerStats.strength} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('str')} style={this.addPoint}>+</span> : null}</h3>
+                    <h3>{this.props.playerStats.wisdom} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('wis')} style={this.addPoint}>+</span> : null}</h3>
+                    <h3>{this.props.playerStats.agility} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('agi')} style={this.addPoint}>+</span> : null}</h3>
+                    <h3>{this.props.playerStats.constitution} {this.props.playerStats.statPoints !== 0 ? <span onClick={() => this.handleStatDistribution('cons')} style={this.addPoint}>+</span> : null}</h3>
+                  </Col>
+                </div>
+              ) : this.state.viewedTab === 'details' ? (
+                <div>
+                  <h2>Attack power</h2>
+                  <h3>{this.props.playerStats.attackPowerMin} - {this.props.playerStats.attackPowerMax}</h3>
+                  <h2>Dodge chance</h2>
+                  <h3>{this.props.playerStats.baseDodgeChance}%</h3>
+                  <h2>Armour</h2>
+                  <h3>{this.props.playerStats.armour}</h3>
+                  <p>Damage reduction: {this.props.playerStats.damageReduction}%</p>
+                </div>
+              ) : null}
+
             </Col>
         )
       }
