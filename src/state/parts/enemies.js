2d78ba74b4bc040bfffd0832cebcca5362fcaf4a
Falborski Artur <falborski.artur@gmail.com>
2017-11-17 16:34:13 +0100

    add set pattern action to enemies reducer

--- src/state/parts/enemies.js
+++ src/state/parts/enemies.js
@@ -3,7 +3,8 @@ const initialState = {
   fetching: false,
   error: null,
   enemyRNG: 0,
-  aPatternI: 0
+  aPatternI: 0,
+  pattern: null
 }
 
 export default (state = initialState, action) => {
@@ -25,6 +26,11 @@ export default (state = initialState, action) => {
         fetching: false,
         error: action.error
       }
+    case 'enemies/SET_PATTERN':
+      return {
+        ...state,
+        pattern: action.pattern
+      }
     case 'enemies/SET_ENEMY_RNG':
       return {
           ...state,
@@ -33,7 +39,7 @@ export default (state = initialState, action) => {
     case 'enemies/NEXT_ATT_PATTERN':
       return {
         ...state,
-        aPatternI: state.aPatternI === 5 ? 0 : (state.aPatternI + 1)
+        aPatternI: state.aPatternI === state.pattern.length - 1 ? 0 : (state.aPatternI + 1)
       }
     default:
       return {
